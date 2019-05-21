1c4420ae6da2cee42152701c70cc1d03710d6d73
Bryan Daniel <41975184+bryand2@users.noreply.github.com>
2019-05-21 16:10:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var hostname = os.hostname();
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello world from ' + hostname + '! Your app is up and running in a cluster!\n')
+  res.send('Hello world from ' + hostname + '! Your app is up and running in a cluster! BRYAN WAS HERE!\n')
 })
 app.listen(8080, function() {
   console.log('Sample app is listening on port 8080.')
